3e1f4a622d765f54c48413a4f52b18269e353914
rajaramcomputers <rajaramcomputers@gmail.com>
2020-04-13 13:23:23 +0400

    Last step of the assignment completed Successfully

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,30 @@
+#!/usr/bin/env python3
 import random
 def main():
-  #print("Keep it logically awesome.")
-
+  interested = input("Are you interested to add a Quote Y /N : ")
+  print(interested)
+  while interested in {'Y','y'}:
+    quote_text = input("Enter the Quote: ")
+    filename = "quotes.txt"
+    interested = 'n'
+    with open (filename, 'a') as f:
+      f.write(quote_text)
+      f.write("\n")
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
-  rnd = random.randint(0,last)
+  last = len(quotes)
+  rnd = random.randint(0,last-1)
+  if rnd == 0:
+     nrnd = random.randint(0,last+1)
+  else:
+     nrnd = random.randint(0,last-1)
 
+  print("Here is the Quote for today")
   print(quotes[rnd])
+  print(quotes[nrnd])
+
+  print("Rnd", rnd, "NRnd", nrnd)	
 
 if __name__== "__main__":
   main()
